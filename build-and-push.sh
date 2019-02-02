75e684eeabccf63025d77e5367e72352a10bc5ea
Noam Ross (Travis-CI) <ross@ecohealthalliance.org>
2019-02-02 16:41:48 -0500

    major refactor

---
+++ build-and-push.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+for tag in apt rstan inla rpkg server gpu
+do
+  image=ecohealthalliance/reservoir:$tag
+  docker pull $image || true
+  docker build -f Dockerfile.$tag --cache-from $image -t $image .
+  docker push $image
+done
---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e 
+
+for tag in apt rstan inla rpkg server gpu
+do
+  image=ecohealthalliance/reservoir:$tag
+  docker pull $image || true
+  docker build -f Dockerfile.$tag --cache-from $image -t $image .
+done

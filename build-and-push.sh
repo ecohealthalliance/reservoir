75041bc9018b2b3aaccd5b3a7d7d62c1d7a8875b
Noam Ross (Travis-CI) <ross@ecohealthalliance.org>
2019-02-04 17:08:13 +0000

    Minimize builds of rstan/rcppeigen, remove rJava
    Also don't pull before building it doesn't speed up.

--- build-and-push.sh
+++ build-and-push.sh
@@ -3,10 +3,9 @@
 set -e
 set -x
 
-for tag in apt rstan inla rpkg server gpu
+for tag in apt rcppeigen rstan inla rpkg server gpu
 do
   image=ecohealthalliance/reservoir:$tag
-  docker pull $image || true
   docker build -q -f Dockerfile.$tag --cache-from $image -t $image .
   docker push $image
 done
--- build.sh
+++ build.sh
@@ -3,9 +3,8 @@
 set -e 
 set -x
 
-for tag in apt rstan inla rpkg server gpu
+for tag in apt rcppeigen rstan inla rpkg server gpu
 do
   image=ecohealthalliance/reservoir:$tag
-  docker pull $image || true
   docker build -q -f Dockerfile.$tag --cache-from $image -t $image .
 done

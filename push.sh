21b366fb2eb07172eeb4098f6b7ebc1cda77ca8e
Noam Ross (Travis-CI) <ross@ecohealthalliance.org>
2019-02-11 20:18:33 +0000

    Try local cache

--- push.sh
+++ push.sh
@@ -6,6 +6,5 @@ set -x
 for tag in apt rcppeigen rstan inla rpkg server gpu
 do
   image=ecohealthalliance/reservoir:$tag
-  docker build -q -f Dockerfile.$tag --cache-from $image -t $image .
   docker push $image
 done

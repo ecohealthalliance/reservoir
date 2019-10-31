991711e3db7e0e222d1df873b021f9174c5b25b3
Noam Ross (Travis-CI) <ross@ecohealthalliance.org>
2019-10-31 09:13:40 -0400

    Add tools for git-crypt

--- config/bash_settings.sh
+++ config/bash_settings.sh
@@ -51,3 +51,4 @@ export GUROBI_HOME="/opt/gurobi752/linux64"
 export PATH="${PATH}:${GUROBI_HOME}/bin"
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"
 export EDITOR=/usr/bin/micro
+export GPG_TTY=$(tty)

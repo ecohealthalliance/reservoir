b0b029da4f47e7cc7efe1565475efc3d8b5a9f76
Noam Ross (Travis-CI) <ross@ecohealthalliance.org>
2020-03-08 14:30:14 -0400

    Stop building quietly so there is output for CI

--- build-and-push.sh
+++ build-and-push.sh
@@ -7,6 +7,6 @@ for tag in apt rcppeigen rstan inla rpkg server gpu
 do
   image=ecohealthalliance/reservoir:$tag
   time docker pull $image > /dev/null
-  time docker build -q -f Dockerfile.$tag --cache-from $image -t $image .
+  time docker build -f Dockerfile.$tag --cache-from $image -t $image .
   docker push $image > /dev/null
 done

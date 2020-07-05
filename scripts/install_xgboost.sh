d18838e737e333a96fb1c930ce27426e1e15ad0f
Noam Ross (Travis-CI) <ross@ecohealthalliance.org>
2020-07-05 11:09:58 -0400

    Build xgboost with NCCL

--- scripts/install_xgboost.sh
+++ scripts/install_xgboost.sh
@@ -3,14 +3,18 @@
 set -e
 set -x
 
+NCCL_VERSION=${NCCL_VERSION:-2.4.8-1+cuda10.1}
+
 apt-get update
 apt-get install -y --allow-downgrades --allow-remove-essential --allow-change-held-packages --allow-unauthenticated --no-install-recommends --no-upgrade \
   gcc-8 \
-  g++-8
+  g++-8 \
+  libnccl2=${NCCL_VERSION} \
+  libnccl-dev=${NCCL_VERSION} 
 
 git clone --recursive https://github.com/dmlc/xgboost
 mkdir -p xgboost/build && cd xgboost/build
-CC=gcc-8 CXX=g++-8 cmake .. -DUSE_CUDA=ON -DR_LIB=ON
+CC=gcc-8 CXX=g++-8 cmake .. -DUSE_CUDA=ON -DR_LIB=ON -DUSE_NCCL=ON
 CC=gcc-8 CXX=g++-8 make install
 cd ../.. && rm -rf xgboost
 

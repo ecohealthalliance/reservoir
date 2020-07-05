7112cfef66771996b5932526e3f32beb92c96fc8
Noam Ross (Travis-CI) <ross@ecohealthalliance.org>
2020-07-05 10:45:41 -0400

    compile xgboost with gcc-8

--- scripts/install_xgboost.sh
+++ scripts/install_xgboost.sh
@@ -1,7 +1,12 @@
 # Xgboost
 
+apt-get update
+apt-get install -y --allow-downgrades --allow-remove-essential --allow-change-held-packages --allow-unauthenticated --no-install-recommends --no-upgrade \
+  gcc-8 \
+  g++-8
+
 git clone --recursive https://github.com/dmlc/xgboost
 mkdir -p xgboost/build && cd xgboost/build
-cmake .. -DUSE_CUDA=ON -DR_LIB=ON
-make install
+CC=gcc-8 CXX=g++-8 cmake .. -DUSE_CUDA=ON 
+CC=gcc-8 CXX=g++-8 make 
 cd ../.. && rm -rf xgboost
